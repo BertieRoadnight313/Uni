83618765fc2a78351ec63404233e278c01f66e93
BertieRoadnight313 <bertieroadnight@gmail.com>
2025-02-04 16:01:42 +0000

    Coding tasks from week 1

---
+++ Week1_tasks.py
@@ -0,0 +1,29 @@
+# Task 1
+print("hello world!")
+
+# Task 2
+print("hello Bert!")
+
+# Task 3
+cent = 38.4
+fahr = cent * 9/5 + 32
+print(fahr)
+
+# Task 4
+match = 609
+batted = 1014
+notout = 162
+runs = 48426
+average = runs / (batted - notout)
+print(average)
+
+# Task 5
+students = 100
+left_over = students % 24 #left over students in smaller lab group
+full_groups = students // 24 #how many full lab groups
+print("There are", full_groups, "full lab groups and", left_over, "students left over.")
+
+# Task 6
+name = input("What is your name? ")
+print("Hello", name)
+print(20//3)
